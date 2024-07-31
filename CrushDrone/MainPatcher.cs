3eebee59b4dd1640d0ecbd3f26f4f8552e88b203
Michael Neises <neisesmichael@gmail.com>
2024-07-31 01:51:36 -0500

    Update MainPatcher.cs

--- CrushDrone/MainPatcher.cs
+++ CrushDrone/MainPatcher.cs
@@ -13,7 +13,7 @@ using innateStorage = System.Collections.Generic.List<System.Tuple<System.String
 namespace CrushDrone
 {
     [BepInPlugin("com.mikjaw.subnautica.crush.mod", "CrushDrone", "1.0.0")]
-    [BepInDependency(VehicleFramework.PluginInfo.PLUGIN_GUID, "1.2.0")]
+    [BepInDependency(VehicleFramework.PluginInfo.PLUGIN_GUID, VehicleFramework.PluginInfo.PLUGIN_VERSION)]
     [BepInDependency(Nautilus.PluginInfo.PLUGIN_GUID)]
     public class MainPatcher : BaseUnityPlugin
     {
